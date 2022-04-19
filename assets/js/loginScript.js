d95d4a044545a1f501dd8e0d1c95736fded4c519
aiexgt <lima14florian14@gmail.com>
2022-04-19 14:37:41 -0600

    login terminado

--- assets/js/loginScript.js
+++ assets/js/loginScript.js
@@ -1,4 +1,18 @@
 const buttonLogin = document.querySelector("#ingresar");
+const error = document.querySelector("#error");
+
+const showError = (mensaje) =>{
+    error.innerHTML = "<b>Error: </b>" +mensaje;
+    error.classList.remove("invisible");
+    error.classList.add("error");
+
+    setTimeout(hideError,2000);
+}
+
+const hideError = () =>{
+    error.classList.remove("error");
+    error.classList.add("invisible");
+}
 
 const login = (usuario, password) => {
     $.post("backend/login.php",{
@@ -7,12 +21,22 @@ const login = (usuario, password) => {
     }, (data, status) => {
         if(data == 1){
             window.location.href = "menu.html";
+        }else{
+            showError("Usuario o contraseña incorrectas");
         }
     })
 }
 
 buttonLogin.addEventListener('click', (e) =>{
     e.preventDefault();
-    login(document.querySelector("#usuario").value,
-    document.querySelector("#password").value);
+    let usuario = document.querySelector("#usuario").value;
+    let password = document.querySelector("#password").value;
+    hideError();
+    if(usuario == ""){
+        showError("Ingrese usuario");
+    }else if(password == ""){
+        showError("Ingrese contraseña");
+    }else{
+        login(usuario, password);
+    }
 })
