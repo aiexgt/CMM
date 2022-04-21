2fab06882cfe1e879a8edd4046b6fdd02c408762
aiexgt <lima14florian14@gmail.com>
2022-04-21 16:22:57 -0600

    empresas

--- assets/js/empresas/script.js
+++ assets/js/empresas/script.js
@@ -1,7 +1,17 @@
 function mostrar() {
-    $.post("backend/ajax/empresas/mostrar.php", {}, function (data, status) {
+    $.post("backend/ajax/empresas/mostrarEmpresas.php", {}, (data, status) => {
         document.querySelector("#tabla-contenido").innerHTML = data;
     });
 }
 
-mostrar();
+const nuevo = document.querySelector("#btn-nuevo");
+
+nuevo.addEventListener('click', () => {
+    $.post("backend/ajax/empresas/mostrarPaises.php", {}, (data, status) => {
+        document.querySelector("#pais").innerHTML = data;
+    })
+})
+
+$(document).ready(() => {
+    mostrar();
+});
