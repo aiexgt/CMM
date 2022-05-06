aae9f321a4e9b7dda7060f750c117178cb13efd0
aiexgt <lima14florian14@gmail.com>
2022-05-06 13:53:56 -0600

    Copiar ausencia a vacaciones

---
+++ backend/ajax/vacaciones/eliminarAusencia.php
@@ -0,0 +1,20 @@
+<?php
+
+//* Validación contenido codigo
+if(isset($_POST['codigo']) && isset($_POST['codigo']) != "")
+{
+    //* Enlace BD
+    include("../../conexion.php");
+
+    //* Recepción variables
+    $codigo = $_POST['codigo'];
+
+    $query = "DELETE FROM ausencias WHERE id = $codigo";
+    if (!$result = mysqli_query($con, $query)) {
+            exit(mysqli_error($con));
+    }
+
+    //* Imprimir resultado
+    echo $result;
+}
+?>
