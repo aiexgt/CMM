bf78f771d0ff41b01a68acecde27fd49557534c0
aiexgt <lima14florian14@gmail.com>
2022-05-13 13:44:41 -0600

    quitar empresa de trabajador

--- backend/ajax/vacaciones/mostrarTrabajadores.php
+++ backend/ajax/vacaciones/mostrarTrabajadores.php
@@ -6,7 +6,7 @@
 
 	$data = '<option value="0" selected>Seleccione Trabajador</option>';
 
-	$query = "SELECT p.id, p.nombre, p.apellido, (SELECT e.nombre FROM empresas e WHERE e.id = p.empresa_id) AS empresa FROM personas p 
+	$query = "SELECT p.id, p.nombre, p.apellido FROM personas p 
 	WHERE p.empresa_id = $empresa AND p.estado_trabajo_id != 3 ORDER BY p.id ASC";
 
 	if (!$result = mysqli_query($con, $query)) {
@@ -18,7 +18,7 @@
     {
     	while($row = mysqli_fetch_assoc($result))
     	{
-    		$data .= '<option value="'.$row["id"].'">'.$row["nombre"]." ".$row["apellido"]." - ".$row["empresa"].'</option>';
+    		$data .= '<option value="'.$row["id"].'">'.$row["nombre"]." ".$row["apellido"].'</option>';
     	}
     }
 
