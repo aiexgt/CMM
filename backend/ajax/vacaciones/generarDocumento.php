cd40689548b17e136d9337f6d3e54b4c37e53dac
aiexgt <lima14florian14@gmail.com>
2022-06-07 11:13:43 -0600

    error

--- backend/ajax/vacaciones/generarDocumento.php
+++ backend/ajax/vacaciones/generarDocumento.php
@@ -21,4 +21,39 @@ if(mysqli_num_rows($result) > 0)
     {
     $row = mysqli_fetch_assoc($result);
     }
+
+require('../../../assets/imported/fpdf184/fpdf.php');
+
+// create document
+$pdf = new FPDF();
+$pdf->AddPage();
+    
+// config document
+$pdf->SetTitle('Generar archivos PDF con PHP');
+$pdf->SetAuthor('Kodetop');
+$pdf->SetCreator('FPDF Maker');
+    
+// add title
+$pdf->Image('../../../img/logo-empresas/'.$row['empresa_id'].'.jpg   ', 68, null, 75, null,'jpg');
+$pdf->SetFont('Arial', 'B', 24);
+$pdf->Cell(0, 10, $row['empresa'] , 0, 1);
+$pdf->SetFont('Arial', 'B', 18);
+$pdf->Cell(0, 10, "Solicitud de Vacaciones" , 0, 1);
+$pdf->Ln();
+    
+// add text
+$pdf->SetFont('Arial', '', 12);
+$pdf->MultiCell(0, 7, utf8_decode('Fecha: '.$fecha), 0, 1);
+$pdf->MultiCell(0, 7, utf8_decode('Nombre del empleado: '.$row['nombre'].' '.$row['apellido']), 0, 1);
+$pdf->MultiCell(0, 7, utf8_decode('Cargo que desempeña: '.$row['puesto']), 0, 1);
+$pdf->Ln();
+$pdf->MultiCell(0, 7, utf8_decode('Por medio de la presente, solicito me sea autorizado '.$cantidad.' días(s) a cuenta de mis vacaciones correspondientes al periodo del 2022, conforme al programa de vacaciones del personal que la empresa fije en atención a las necesidades de la actividad de la misma, para ser gozados del '.$fecha_inicio.' al '.$fecha_fin.'.'), 0, 1);
+$pdf->Ln();
+    
+// add image
+
+    
+// output file
+$pdf->Output('F', 'fpdf-complete.pdf');
+
 ?>
