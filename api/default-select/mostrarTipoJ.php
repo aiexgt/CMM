96aec3017c382968325da8a146134d9de99654aa
aiexgt <lima14florian14@gmail.com>
2022-06-24 09:31:56 -0600

    select tipo

---
+++ api/default-select/mostrarTipoJ.php
@@ -0,0 +1,23 @@
+<?php
+	//* Enlace BD
+	include("../conexion.php");
+
+	$data = '<option value="0" selected>Selecciones Tipo</option>';
+
+	$query = "SELECT id, nombre FROM tiposJ ORDER BY id ASC";
+
+	if (!$result = mysqli_query($con, $query)) {
+        exit(mysqli_error($con));
+    }
+
+    // if query results contains rows then featch those rows 
+    if(mysqli_num_rows($result) > 0)
+    {
+    	while($row = mysqli_fetch_assoc($result))
+    	{
+    		$data .= '<option value="'.$row["id"].'">'.$row["nombre"].'</option>';
+    	}
+    }
+
+    echo $data;
+?>
